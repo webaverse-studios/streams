c2e72e32662778102d750fb4a1506637672d60b4
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2023-02-14 13:39:32 -0800

    rename saveBlob to saveStream

--- saveStream.js
+++ saveStream.js
@@ -12,13 +12,13 @@ const types = [
 
 
 /**
- * Save a blob by piping it to a file.
+ * Save a stream by piping it to a file.
  * @param {ReadableStream} stream The stream to save.
  * @param {string} filename The suggested filename.
  * @param {string} id The file picker ID.
  * @returns {Promise<FileSystemFileHandle>} The file handle.
  */
-export async function saveBlob(
+export async function saveStream(
   stream = new ReadableStream(),
   filename = 'file.gz',
   id = 'download',
